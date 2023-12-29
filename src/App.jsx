1dda4147d436a6cbc4e22635a3b40f289c66d894
Dmitry <dmitry27zh@gmail.com>
2023-12-29 18:43:46 +0500

    Extract Block

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
 import './App.css'
+import Block from './Block'
 
 function App() {
   return (
     <div className="App">
-      <div className="block">
-        <ul className="currencies">
-          <li className="active">USD</li>
-          <li>EUR</li>
-          <li>RUB</li>
-          <li>
-            <svg height="50px" viewBox="0 0 50 50" width="50px">
-              <rect fill="none" height="50" width="50" />
-              <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
-            </svg>
-          </li>
-        </ul>
-        <input type="number" placeholder={0} />
-      </div>
-      <div className="block">
-        <ul className="currencies">
-          <li className="active">USD</li>
-          <li>EUR</li>
-          <li>RUB</li>
-          <li>
-            <svg height="50px" viewBox="0 0 50 50" width="50px">
-              <rect fill="none" height="50" width="50" />
-              <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
-            </svg>
-          </li>
-        </ul>
-        <input type="number" placeholder={0} />
-      </div>
+      <Block />
+      <Block />
     </div>
   )
 }
---
+++ src/Block.jsx
@@ -0,0 +1,20 @@
+const block = () => {
+  return (
+    <div className="block">
+      <ul className="currencies">
+        <li className="active">USD</li>
+        <li>EUR</li>
+        <li>RUB</li>
+        <li>
+          <svg height="50px" viewBox="0 0 50 50" width="50px">
+            <rect fill="none" height="50" width="50" />
+            <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
+          </svg>
+        </li>
+      </ul>
+      <input type="number" placeholder={0} />
+    </div>
+  )
+}
+
+export default block
